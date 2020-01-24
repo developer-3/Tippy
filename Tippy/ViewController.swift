68982b6d2eb0238adfbc591363005caa23b37e75
Adam Anderson <adamcanderson400@gmail.com>
2020-01-24 11:20:59 -0500

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Adam Anderson on 1/24/20.
+//  Copyright © 2020 Adam Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
